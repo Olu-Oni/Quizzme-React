82657a0982670e4236e0543550c426c295ed508f
Olu-Oni <oluomicheal8@gmail.com>
2025-09-09 14:25:23 +0100

    performance layout update

--- src/pages/main/Performance/Perfomance.jsx
+++ src/pages/main/Performance/Perfomance.jsx
@@ -2,7 +2,9 @@ const Performance = () => {
 
   return (
     <div className="baloo flex flex-col justify-center px-4 sm:px-10 md:px-14 lg:px-[10%]">
-      Nothing to see here... yet ;)
+      <h1 className="mt-6 text-center md:mt-12">
+        Nothing to see here... <br/> yet ;)
+        </h1>
     </div>
   );
 };
