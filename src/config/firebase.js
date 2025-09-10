baf4a7265a25bffefbf3d8433036fcd4a51d046e
Olu-Oni <oluomicheal8@gmail.com>
2025-09-10 22:25:39 +0100

    test change

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,11 +4,11 @@ import { getFirestore} from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET ,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID ,
-  appId: import.meta.env.VITE_APP_ID
+  authDomain: "quizzme1.firebaseapp.com",
+  projectId: "quizzme1",
+  storageBucket: "quizzme1.firebasestorage.app",
+  messagingSenderId: "917250227968",
+  appId:"1:917250227968:web:c89e9232fc243777557de5"
 };
 
 // Initialize Firebase
